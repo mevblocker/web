3a468a1dc3bbb90810831fcccfa25c7a3cb9e734
nenadV91 <nenadwebdevelop@gmail.com>
2023-03-23 13:08:18 +0100

    Use env var for GA id

--- src/const/meta.ts
+++ src/const/meta.ts
@@ -1,20 +1,26 @@
 const API_BASE_URL = "https://www.mevblocker.com";
 
 export const CONFIG = {
-  title: 'MEVBlocker.io',
-  titleShort: 'MEVBlocker.io',
-  description: 'MEV Blocker is an RPC that shields all your transactions from MEV, and returns those protected funds directly to you -the user.',
-  descriptionShort: 'MEV Blocker is an RPC that shields all your transactions from MEV.',
-  googleAnalyticsID: 'G-JV2Q6SXRNR',
+  title: "MEVBlocker.io",
+  titleShort: "MEVBlocker.io",
+  description:
+    "MEV Blocker is an RPC that shields all your transactions from MEV, and returns those protected funds directly to you -the user.",
+  descriptionShort:
+    "MEV Blocker is an RPC that shields all your transactions from MEV.",
+  googleAnalyticsID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   url: {
     root: "https://mevblocker.io",
     home: "/",
     faq: "/faq",
   },
   social: {
-    twitter: { label: 'Twitter', account: 'mevblocker', url: 'https://twitter.com/' },
-    discord: { label: 'Discord', url: 'https://discord.gg/4wCETCfpQu' },
-    forum: { label: 'Forum', url: 'https://mevblocker.discourse.group/' },
-    github: { label: 'GitHub', url: 'https://github.com/mevblocker' }
-  }
-}
+    twitter: {
+      label: "Twitter",
+      account: "mevblocker",
+      url: "https://twitter.com/",
+    },
+    discord: { label: "Discord", url: "https://discord.gg/4wCETCfpQu" },
+    forum: { label: "Forum", url: "https://mevblocker.discourse.group/" },
+    github: { label: "GitHub", url: "https://github.com/mevblocker" },
+  },
+};
