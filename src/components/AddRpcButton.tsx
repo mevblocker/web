f3977b09a431e64efcea7a990860515a59cbb44c
Anxo Rodriguez <anxolin@gmail.com>
2023-03-24 12:36:54 +0000

    Add connect button

--- src/components/AddRpcButton.tsx
+++ src/components/AddRpcButton.tsx
@@ -2,9 +2,20 @@ import styled from "styled-components";
 import { transparentize } from "polished";
 import { Defaults, Color, Font, Media } from "@src/const/styles/variables";
 import { BigButton } from "./Button";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const BigButtonWrapper = styled.a``;
 
 export function AddRpcButton() {
-  return <BigButton label="Add to MetaMask" href="https://metamask.io/" />;
+  return (
+    <>
+      <ConnectButton
+        label="Connect Wallet"
+        accountStatus={"avatar"}
+        chainStatus={"none"}
+        showBalance={false}
+      />
+      <BigButton label="Add to MetaMask" href="https://metamask.io/" />;
+    </>
+  );
 }
