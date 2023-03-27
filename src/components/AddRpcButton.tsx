7a966428e75c1e8745ae9fb88cff7169674b8089
Anxo Rodriguez <anxolin@gmail.com>
2023-03-27 14:40:29 +0100

    Handle add rpc

--- src/components/AddRpcButton.tsx
+++ src/components/AddRpcButton.tsx
@@ -2,10 +2,20 @@ import { BigButton } from "./Button";
 // import { ConnectButton, useConnectModal } from "@rainbow-me/rainbowkit";
 import { useAddRpcEndpoint } from "@src/hooks/useAddRpcEndpoint";
 import { Confetti } from "./Confetti";
+import { useCallback } from "react";
+
+
 
 
 export function AddRpcButton() {
   const { addRpcEndpoint, addedRpc } = useAddRpcEndpoint()
+
+  const addToWallet = useCallback(() => {
+    addRpcEndpoint()
+      .then(() => console.log('Success!'))
+      .catch(error => console.error('[AddRpcButton] Error adding RPC to Wallet', error)) // TODO: Handle by open Raimbow Wallet :) 
+  }, [addRpcEndpoint])
+
   // const { openConnectModal } = useConnectModal()
 
   // TODO: Disabled, for now to just go for injected wallet. Will probably improve and use it to support Wallet Connect and other wallets
@@ -24,9 +34,10 @@ export function AddRpcButton() {
         <>
         <Confetti start={true} />
         <span style={{color: 'green'}}>Added to your wallet! You are now safe</span>
+        {/* <a onClick={() => disconnect()}>Disconnect</a> */}
         </>
       ) : (
-        <BigButton onClick={addRpcEndpoint} label="Add to Wallet" />
+        <BigButton onClick={addToWallet} label="Add to Wallet" />
       )}
     </>
   );
