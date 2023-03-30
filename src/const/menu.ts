3a5bd393cec8050aad3d24caefd94acbe3e97cca
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2023-03-30 19:29:35 +0200

    fix relative urls

--- src/const/menu.ts
+++ src/const/menu.ts
@@ -3,12 +3,12 @@ import { openExternalLinkClickedElement, scrollToAction, scrollToClickedElement 
 const {social} = CONFIG
 
 export const mainMenu = [
-  { id: 0, title: 'Get protected', url: CONFIG.url.rpc, onClick: scrollToClickedElement },
-  { id: 1, title: 'FAQ', url: CONFIG.url.faq, onClick: scrollToClickedElement },
+  { id: 0, title: 'Get protected', url: `${CONFIG.url.root}/${CONFIG.url.rpc}`, onClick: scrollToClickedElement },
+  { id: 1, title: 'FAQ', url: `${CONFIG.url.root}/${CONFIG.url.faq}`, onClick: scrollToClickedElement },
 ]
 
 export const footerMenu = [
-  {id: 0, title: 'Documentation', url: CONFIG.url.docs },
+  {id: 0, title: 'Documentation', url: `${CONFIG.url.root}/${CONFIG.url.docs}` },
   {id: 1, title: 'Questions & Support', url: social.telegram.url, target: '_blank', onClick: openExternalLinkClickedElement },
 ]
 
