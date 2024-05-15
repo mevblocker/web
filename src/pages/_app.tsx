378479d4f0057b105aad0b6bafdc64cc2bd02f9a
Anxo Rodriguez <anxolin@gmail.com>
2024-05-15 17:27:06 +0100

    Add appzi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,11 @@ import { AppProps } from 'next/app'
 import { useAnalyticsReporter } from "../lib/analytics";
 import GlobalStyles from '@src/const/styles/global'
 
+import ReactAppzi from 'react-appzi';
 
+
+const APPZI_TOKEN = process.env.REACT_APP_APPZI_TOKEN || 'zpjcs'
+ReactAppzi.initialize(APPZI_TOKEN);
 
 export default function App({ Component, pageProps, router }: AppProps) {
   useAnalyticsReporter({ pathname: router.asPath });
