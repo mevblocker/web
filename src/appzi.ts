0a3a3b57bc7b9468f90b58ef179772a85d41e6e8
Leandro <alfetopito@users.noreply.github.com>
2025-03-14 13:52:58 +0000

    feat: remove all content (#70)
    * chore: update readme
    
    * feat: remove all content

--- src/appzi.ts
+++
@@ -1,6 +0,0 @@
-import ReactAppzi from 'react-appzi';
-
-if (typeof window !== 'undefined') {
-  const APPZI_TOKEN = process.env.REACT_APP_APPZI_TOKEN || 'zpjcs'
-  ReactAppzi.initialize(APPZI_TOKEN);
-}
