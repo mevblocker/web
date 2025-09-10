d5cdec19eec412eafaf71b90ce6a8c81d1992cd3
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2025-09-10 15:28:20 +0100

    feat: implement redirection for /docs and update related links

--- src/const/meta.ts
+++ src/const/meta.ts
@@ -1,12 +1,12 @@
 export const CONFIG = {
   title: "MEV Blocker",
-  titleShort: "MEV Blocker",  
+  titleShort: "MEV Blocker",
   description:
     "MEV Blocker is a fast and free RPC endpoint that protects users from frontrunning and sandwich attacks on a wide range of Ethereum transactions.",
   descriptionShort: "Everyday protection from harmful MEV",
   socialShareDescription:
     "I'm using MEV Blocker to protect myself from frontrunning and sandwich attacks. You should too. Set up the RPC endpoint here:",
-  walletConnectProjectId: "fccf33f4fcf41c93e5144e5b677edd32", 
+  walletConnectProjectId: "fccf33f4fcf41c93e5144e5b677edd32",
   rpc: {
     url: "https://rpc.mevblocker.io",
     chainId: 1,
@@ -20,7 +20,7 @@ export const CONFIG = {
     home: "/",
     faq: "#faq",
     rpc: "#rpc",
-    docs: "/docs",
+    docs: "https://docs.cow.fi/mevblocker?utm_source=cow.fi&utm_medium=web&utm_content=mev-blocker-docs-link",
     cowSwap: "https://swap.cow.fi",
     cowProtocol: "https://cow.fi",
     beaver: "https://beaverbuild.org/",
@@ -32,4 +32,4 @@ export const CONFIG = {
   social: {
     telegram: { label: "Telegram", url: "https://t.me/+yonLSGoFPRI0YTFk" },
   },
-};
+}
--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,11 +1,12 @@
-import Layout from "@src/components/Layout/index";
+import type { GetServerSideProps } from "next"
+import Layout from "@src/components/Layout/index"
 import {
   Section,
   SectionContent,
   SectionWrapper,
-} from "@src/const/styles/pages/index";
-import { Color } from "@src/const/styles/variables";
-import Docs from "@src/markdown/docs.md";
+} from "@src/const/styles/pages/index"
+import { Color } from "@src/const/styles/variables"
+import Docs from "@src/markdown/docs.md"
 
 export default function DocsPage() {
   return (
@@ -16,5 +17,15 @@ export default function DocsPage() {
         </Section>
       </SectionWrapper>
     </Layout>
-  );
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination:
+        "https://docs.cow.fi/mevblocker?utm_source=cow.fi&utm_medium=web&utm_content=mev-blocker-docs-link",
+      permanent: false,
+    },
+  }
 }
